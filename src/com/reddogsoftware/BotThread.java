6b41c649ca0f7a505e1063ba77b5e78adeb85cda
mbresnan1701 <bresnan.mw@gmail.com>
2018-08-31 18:55:41 -0700

    work work work

--- src/com/reddogsoftware/BotThread.java
+++ src/com/reddogsoftware/BotThread.java
@@ -1,23 +1,41 @@
 package com.reddogsoftware;
-import java.io.IOException;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.InterruptedException;
+import java.lang.ProcessBuilder;
 
 class BotThread implements Runnable {
-    private static final String scriptPath = "c:\\\\development\\\\pycraft\\\\starcraft_bot\\main.py";
+    private static final String pythonExecutable = "C:/Users/Captobviouz/Anaconda3/python.exe";
+    private static final String scriptPath = "C:/Development/pycraft/starcraft_bot/main.py";
+
     public void run() {
         try {
             this.runPython();
         } catch (IOException e) {
-            System.out.println("IO Exception thread");
+            System.out.println("IO Exception");
         } catch (InterruptedException e) {
-            System.out.println("Interrupted thread");
+            System.out.println("Interrupted");
         }
     }
 
     private void runPython() throws IOException, InterruptedException {
-        ProcessBuilder pb = new ProcessBuilder("python", scriptPath);
+        System.out.println("Calling runPython");
+        ProcessBuilder pb = new ProcessBuilder(pythonExecutable, scriptPath);
         Process p = pb.start();
-        p.waitFor();
+        BufferedReader bfr = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line = "";
+        System.out.println("Running Python starts: " + line);
+        int exitCode = p.waitFor();
+        System.out.println("Exit Code : " + exitCode);
+        line = bfr.readLine();
+        System.out.println("First Line: " + line);
+        while ((line = bfr.readLine()) != null) {
+            System.out.println("Python Output: " + line);
+
+
+        }
         this.runPython();
     }
 }
--- src/com/reddogsoftware/Main.java
+++ src/com/reddogsoftware/Main.java
@@ -1,10 +1,12 @@
 package com.reddogsoftware;
 
 public class Main {
-
+    private static final int maxThreads = 10;
     public static void main(String[] args) {
-        BotThread botThread = new BotThread();
-        Thread t = new Thread(botThread);
-        t.start();
+        for(int i = 0; i < maxThreads; i++) {
+            BotThread botThread = new BotThread();
+            Thread t = new Thread(botThread);
+            t.start();
+        }
     }
 }
